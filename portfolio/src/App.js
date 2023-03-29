e35a78da25cddbd99d1867f9440594142a18187a
eltuzzador <spiderhp99@gmail.com>
2023-03-29 21:01:14 +0200

    page uploaded commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO <code>WORLD</code>.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
